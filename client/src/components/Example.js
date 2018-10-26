4691552b59fbb7a379bb071f0101439ec265ff03
Ethan Olson <ethan.olson@libertymutual.com>
2018-10-26 15:58:16 -0500

    Testing Merge

--- client/src/components/Example.js
+++ client/src/components/Example.js
@@ -3,12 +3,13 @@ import React, { Component } from 'react';
 class Example extends Component {
 
     state = {
-        counter: 0
+        counter: 'Evil merge guy mcgee'
     };
   render() {
     return (
-      <div>
+      <div className="ChuckTesta">
           {this.state.counter}
+          <h1>Something else</h1>
       </div>
     );
   }
